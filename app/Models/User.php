0d9ee64bf83f445180fd256d9fdbc660786d039e
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2023-07-26 09:00:23 +0700

    rebase chapter 4 with main

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,46 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Scopes\ActiveScope;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * Get the user's full name.
+     */
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . " " . $this->last_name;
+    }
+
+    /**
+     * Mutate username.
+     */
+    public function setUsernameAttribute($value)
+    {
+        $this->attributes['username'] = Str::slug($value);
+    }
+
+    /**
+     * Create local scope, get admin infor.
+     */
+    public function scopeAdmin($query)
+    {
+        return $query->where('is_admin', true);
+    }
+
+    /**
+     * Create global scope, get active user.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new ActiveScope);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
@@ -35,4 +71,6 @@ class User extends Authenticatable
     protected $guarded = [
         'is_admin',
     ];
+
+    protected $appends = ['username'];
 }
