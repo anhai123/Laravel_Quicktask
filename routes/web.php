04231de497d79dfcfb6eb313e152246c39f9bb53
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2023-07-27 07:13:32 +0700

    chapter 8 ++

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('tasks', TaskController::class);
+
 Route::get('/', function () {
     return response()->json([
         'stuff' => phpinfo()
        ]);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
