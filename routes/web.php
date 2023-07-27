e1f77356c7917ca13c1acd26a68c0501b860a860
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2023-07-27 08:53:07 +0700

    chapter 7

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 });
 Route::resource('tasks', TaskController::class);
 Route::prefix('/users')
+    ->middleware('admin')
     ->controller(UserController::class)
     ->group(function () {
         Route::get('/', 'index')->name('users.index');
